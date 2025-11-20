a39f9cea9c53bf0b8d55d319870ca081584c25c7
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-20 21:12:10 +0600

    Operator

---
+++ Operators/type_Operators.py
@@ -0,0 +1,74 @@
+##Comparison Operator
+num1 =  10
+num2 = 20
+
+print(num1 == num2)
+print(num1 !=  num2)
+print(num1< num2)
+print(num1 >  num2)
+
+ ##Assignment Operator =>> 
+x= 5
+x += 5
+print(x)
+x -= 2
+print(x)
+x *= 2
+print(x)
+x /= 2
+print(x)
+x //= 2
+print(x)
+
+##Logical Operators
+
+print(True and True)
+print(True and False)
+print(False and False)
+
+##OR
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+## Not
+
+print( not True)
+print( not 5 > 7)
+
+## XOR
+
+print(True ^ True)
+print(True ^ False)
+
+## Bitwise Operators
+
+print(num1 & num2 )
+print(num1 | num2 )
+
+##Unary -> Identity ->  Membership -> Operator 
+
+num =  5
+
+print(num << 3)
+
+
+## Identity 
+
+y = 5
+z =  5
+print(y is z)
+
+arr1 = [1,2,3]
+arr2 = [1,2,3]
+
+print(arr1 is not arr2)
+print(arr1 is arr2)
+
+## Membership
+
+my_arr = [1,2,3,4,5,6]
+
+print(10 in my_arr)
+print(10 not in my_arr)
